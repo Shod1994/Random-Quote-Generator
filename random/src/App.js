cacd8407b3a7c5a2101db814a20aaf9eb499f676
Lashod Jones <lashodj@gmail.com>
2024-10-15 00:53:14 -0400

    Starting React App Conversion

---
+++ random/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import '../src/assets/newyorkbg.jpg'
+import { QuoteGen } from '../src/components/QuoteGen'
+
+function App() {
+  return (
+    <div className="App">
+      <QuoteGen />
+    </div>
+  );
+}
+
+export default App;
---
+++ random/src/components/QuoteGen.js
@@ -0,0 +1,23 @@
+
+
+export const QuoteGen = () => {
+    return (
+        <div> <div class="background"></div>
+            <div class="container">
+                <div class="display">
+                    <h1>See Quote Here:</h1>
+                    <p> </p>
+                </div>
+                <div>
+                    <button class="quote-btn" type="button">Random Quote</button>
+                </div>
+
+            </div>
+            <footer>
+                <small>✍️Created By Lashod Jones✍️</small>
+            </footer>
+
+
+        </div>
+    )
+}
